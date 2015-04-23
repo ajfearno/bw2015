e0adfcca65c3b38cb9510a23c6f09150e486ca3a
ajfearno <ajfearno@gmail.com>
2015-04-23 18:50:43 +1200

    Revert "Revert "commit""
    This reverts commit 4853208302265862f8ae78cd359985cfd18a65e7.

---
+++ BrookfieldMidwivesApp/Models/ContactModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BrookfieldMidwivesApp.Models
+{
+    public class ContactModels
+    {
+        
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
